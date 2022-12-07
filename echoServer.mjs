6f6f32352df672a6927bd2a201de441cf140fb95
Ondrej Strnad <ondrejstrnad@gmail.com>
2022-12-07 16:57:29 +0300

    storing of posts

--- echoServer.mjs
+++ echoServer.mjs
@@ -2,9 +2,10 @@ import express from 'express';
 import { argv } from 'process';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import fs from 'fs'
 
 const app = express(express.json());
-const port = argv[2] || 8080;
+const port = argv[2] || 8081;
 
 var newRequest = false;
 var latestRequest = {};
@@ -14,6 +15,19 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json())
 app.use(cors());
 
+function getDate()
+{
+    let d = new Date();
+
+    let day = d.getDate();    
+    let month = d.getMonth();        
+    let year = d.getFullYear();
+    let hours = d.getHours();
+    let minutes = d.getMinutes();
+    let seconds = d.getSeconds();
+    
+    return "" + year + month + day + hours + minutes + seconds;
+}
 
 app.use(express.static('static'));
 
@@ -27,11 +41,11 @@ app.get('/events', async (req, res) => {
     res.flushHeaders();
 
     // Tell the client to retry every 10 seconds if connectivity is lost
-    res.write('retry: 10000\n\n');
+    res.write('retry: 100\n\n');
     let count = 0;
 
     while (true) {
-        await new Promise(resolve => setTimeout(resolve, 1000));
+        await new Promise(resolve => setTimeout(resolve, 100));
 
         // Send a SSE with new commands only when there are new commands for client
         if (newRequest) {
@@ -42,6 +56,34 @@ app.get('/events', async (req, res) => {
     }
 });
 
+app.get('/posts', async (req, res) => {    
+    fs.readdir("posts", (err, files) => {
+        var obj = {};
+        obj.count = files.length;
+        obj.filenames = [];
+        obj.files = [];
+        
+        files.forEach(file => {
+          obj.filenames.push(file);
+          
+          const content = fs.readFileSync("posts/" + file, {encoding:'utf8', flag:'r'});
+          obj.files.push(content);
+
+        });
+
+        var strres = JSON.stringify(obj);
+
+        res.set({
+            'Cache-Control': 'no-cache',
+            'Content-Type': 'application/json',        
+            'Content-Length': strres.length
+        });
+        res.flushHeaders();
+
+        res.write(strres);
+      });
+});
+
 app.post('/', function (req, res) {
   console.log(req.body);
   res = res.status(200);
@@ -50,6 +92,13 @@ app.post('/', function (req, res) {
     res = res.type(req.get('Content-Type'));
   }
   latestRequest = req.body;
+
+  fs.writeFile("posts/post_" + getDate() + ".json", JSON.stringify(req.body), function(err) {
+    if(err) {
+        return console.log(err);
+    }    
+    }); 
+
   newRequest = true;
   res.send(req.body);
 });
